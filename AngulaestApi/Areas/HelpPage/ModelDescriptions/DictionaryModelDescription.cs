b8d2e033ca459c4fca48f1ea144c931c905c5fe4
lijo <lijo.8893@gmail.com>
2017-10-21 09:32:39 +0530

    Add project files.

---
+++ AngulaestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngulaestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AngulaestApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngulaestApi.WebApiApplication" Language="C#" %>
